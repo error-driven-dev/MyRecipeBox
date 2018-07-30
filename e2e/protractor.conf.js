8b2345c2095b9d16cb8be429c0ad0cf9cee29170
Beth Hart <bethhart27@gmail.com>
2018-07-30 23:18:38 +0000

    initial commit

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -12,7 +12,7 @@ exports.config = {
     'browserName': 'chrome'
   },
   directConnect: true,
-  baseUrl: 'http://localhost:4200/',
+  baseUrl: 'https://my-recipes-errordrivendev.c9users.io/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
